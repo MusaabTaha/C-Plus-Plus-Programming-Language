cc568e2c03ce35a56710252e8dbd7467d3d73251
MusaabTaha <104390876+MusaabTaha@users.noreply.github.com>
2023-07-14 20:15:39 +0200

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,13 @@
+//This is a simple challenge to help you practice printing to stdout. 
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello, World!";
+    return 0;
+    
+}
