d3fb5e270f3468ce837c219c3530927f40d30de3
MusaabTaha <104390876+MusaabTaha@users.noreply.github.com>
2023-08-11 15:55:55 +0200

    Create integers_floats.cpp

---
+++ Basics/integers_floats.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdint>
+
+int main(){
+
+    std::int8_t t1 = 0;
+    std::uint8_t t2 = 0;
+
+    std::int16_t t1 = 0;
+    std::uint16_t t2 = 0;
+
+    std::int32_t t1 = 0;
+    std::uint32_t t2 = 0;
+
+    std::int64_t t1 = 0;
+    std::uint64_t t2 = 0;
+
+    float f1 = 0.0F;
+    double f2 = 1;
+
+    bool b = true;
+
+
+    return 0;
+  
+}
