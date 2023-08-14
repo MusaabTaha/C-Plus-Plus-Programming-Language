e51a677fac45682d82f854c21a93c7164cea5e16
MusaabTaha <104390876+MusaabTaha@users.noreply.github.com>
2023-08-14 16:44:17 +0200

    Create Typedef and Using.cpp

---
+++ STL Container/Typedef and Using.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+
+//C-style typdef
+typedef std::vector<std::int8_t> ByteArray;
+
+//C++-style alias
+template <typename T>
+using genericType = std::vector<T>;
+
+
+int main()
+{
+
+    return 0;
+}
